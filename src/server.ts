d489bc894aa6b0966c4d8245f3aa00822a760ddc
Hamid47hrs <hamid47.hrs@gmail.com>
2025-09-13 15:15:17 +0330

    feat(production) Implement prodduction rediness
    - Add helmet and cors middleware to enhance security and prepare for deployment.

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,17 @@ import apiRoutes from "./api/index.routes";
 import dotenv from "dotenv";
 import errorHandler from "./middleware/errorHandler";
 import { setupSwagger } from "./config/swagger";
+import helmet from "helmet";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 
 const PORT: number = Number(process.env.PORT || 3000);
+
+app.use(helmet());
+app.use(cors());
 
 app.use(express.json());
 
